05810ed4111c4c0d7c210218e8aa467096c17755
gibbonplusplus <96675256+gibbonplusplus@users.noreply.github.com>
2021-12-28 16:36:13 -0800

    fixed button
    Fixed button, added main manager

---
+++ PT Project/Assets/MainManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+public class MainManager : MonoBehaviour
+{
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
+    }
+
+}
